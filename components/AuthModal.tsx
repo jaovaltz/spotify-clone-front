13accdc4e22dcf4b7ef8d65bef807d6405c00a7d
joao.valduga <joao.valduga@angellira.com.br>
2023-09-13 09:13:10 -0300

    chore: changed auth modal to be auth with email

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -41,7 +41,6 @@ export function AuthModal() {
       <Auth
         theme="dark"
         magicLink
-        providers={["google", "spotify"]}
         supabaseClient={supabaseClient}
         appearance={{
           theme: ThemeSupa,
